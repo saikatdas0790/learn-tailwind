87271c02bbda66ae24e0acc92172ed8ceb6d173c
Saikat Das <saikatdas0790@gmail.com>
2021-02-09 14:18:28 +0530

    responsive design

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -8,6 +8,9 @@
       <a href="/core-concepts">Core Concepts</a>
       <ul>
         <li><a href="/core-concepts/utility-first">Utility First</a></li>
+        <li>
+          <a href="/core-concepts/responsive-design">Responsive Design</a>
+        </li>
       </ul>
     </li>
   </ul>
